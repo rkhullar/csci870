f95c06c59c32e10809d4692976851072146d0539
Rajan Khullar <rkhullar@nyit.edu>
2016-09-25 19:20:03 -0400

    combine repos

--- config.py
+++
@@ -1,3 +0,0 @@
-from os import environ
-DB = {'database': environ['DB_NAME'], 'user': environ['DB_USER'], 'host': 'localhost', 'password': environ['DB_PSWD']}
-MAIL = {'srvr': 'smtp.gmail.com', 'user': environ['MAIL_USER'], 'pswd': environ['MAIL_PSWD']}
