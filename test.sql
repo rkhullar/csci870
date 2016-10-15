e634412cd594bf6cb755160e2f46197d4132eee0
Rajan Khullar <rkhullar@nyit.edu>
2016-10-15 02:08:17 -0400

    dev

--- test.sql
+++ test.sql
@@ -4,9 +4,7 @@
  * @updated : 10/10/16
  */
 
-
 select new.scan(1473328800, 1, '00:00:00:00:00:00'::macaddr, -100::smallint, 1);
-
 select * from dbv.scan;
 
 /*insert into dbo.scan(select now()::timestamp, '00:00:00:00:00:00'::macaddr, -100);*/
