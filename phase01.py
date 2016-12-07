4d36a0ce5eddfc30550df02f503109d0761ed3a2
Rajan Khullar <rkhullar@nyit.edu>
2016-12-07 17:25:52 -0500

    dev

--- phase01.py
+++ phase01.py
@@ -7,69 +7,34 @@
 """
 
 import csv
-from support import cntx, stat, dec, SCANS
+from support import api, mod
+from support import SCANS, MODES, ATTRS
 
-memL  = ['building', 'floor', 'room']
-memLT = ['building', 'floor', 'room', 'hour']
+cnt = api.cnts()
 
-class analyzer:
-    def __init__(self):
-        self.cntW  = None; self.fltW  = []; self.datW  = {}
-        self.cntT  = None; self.fltT  = []; self.datT  = {}
-        self.cntL  = None; self.fltL  = []; self.datL  = {}
-        self.cntLT = None; self.fltLT = []; self.datLT = {}
+flt = mod.vectorlist()
+for a in ATTRS:
+    z = ATTRS[a]
+    for x in cnt[a]:
+        flt[a].append(mod.objectslice(x, z))
 
-    def request(self):
-        self.cntW = cntx('W')
-        self.cntT = cntx('T')
-        self.cntL = cntx('L')
-        self.cntLT = cntx('LT')
-        for x in self.cntW:
-            self.fltW.append(x.bssid)
-        for x in self.cntT:
-            self.fltT.append(x.hour)
-        for x in self.cntL:
-            d = stat.objectslice(x, memL)
-            self.fltL.append(d)
-        for x in self.cntLT:
-            d = stat.objectslice(x, memLT)
-            self.fltLT.append(d)
+dat = mod.vectordict()
+for m in MODES:
+    for f in flt[m]:
+        dat[m][f] = []
 
-    def prep(self):
-        for w in self.fltW:
-            self.datW[w] = []
-        for t in self.fltT:
-            self.datT[t] = []
-        for l in self.fltL:
-            self.datL[l] = []
-        for lt in self.fltLT:
-            self.datL[lt] = []
+with open(SCANS) as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=';')
+    for r in reader:
+        r['hour'] = 0
+        r['day'] = 0
+        r['quarter'] = 0
+        for m in MODES:
+            z = ATTRS[m]
+            k = mod.dict2object(r, z)
+            if k in flt[m]:
+                dat[m][k].append(r)
 
-    def filter(self):
-        with open(SCANS) as csvfile:
-            reader = csv.DictReader(csvfile, delimiter=';')
-            for r in reader:
-
-                w = r['bssid']
-                if w in self.fltW:
-                    self.datW[w].append(r)
-
-                l = stat.dict2object(r, memL)
-                if l in self.fltL:
-                    self.datL[l].append(r)
-
-def test01():
-    for x in cntx('L'):
-        print(x.building, x.floor, x.room, x.count)
-
-def test02():
-    a = analyzer()
-    a.request()
-    a.prep()
-    a.filter()
-    for w in a.datW:
-        print(w, len(a.datW[w]))
-
-if __name__ == '__main__':
-    #test01()
-    test02()
+for f in flt['W']:
+    n = len(dat['W'][f])
+    print(f.bssid, n)
--- support.py
+++ support.py
@@ -10,8 +10,14 @@ import requests, json, csv
 
 BASEURL = 'https://csci870.nydev.me/api'
 SCANS = 'data/scans.csv'
-
-cntx = lambda m: stat.parse(m, api.count(m))
+MODES = ['W', 'L', 'T', 'TT', 'LT']
+ATTRS = {
+    'W': ['bssid'],
+    'L': ['building', 'floor', 'room'],
+    'T': ['hour'],
+    'TT': ['day', 'hour', 'quarter'],
+    'LT': ['building', 'floor', 'room', 'hour']
+}
 
 class api:
     @staticmethod
@@ -21,16 +27,41 @@ class api:
         #print(r.content.decode('UTF-8'))
         return r.json()
 
-class stat:
+    @staticmethod
+    def cntx(m):
+        return mod.parse(m, api.count(m))
+
+    @staticmethod
+    def cnts():
+        d = {}
+        for m in MODES:
+            d[m] = api.cntx(m)
+        return d
+
+class mod:
     def __init__(self, **kwargs):
         for key, val in kwargs.items():
             setattr(self, key, val)
 
     @staticmethod
+    def vectorlist():
+        d = {}
+        for m in MODES:
+            d[m] = []
+        return d
+
+    @staticmethod
+    def vectordict():
+        d = {}
+        for m in MODES:
+            d[m] = {}
+        return d
+
+    @staticmethod
     def parse(mode, ja):
         l = []
         for jo in ja:
-            x = stat(**jo)
+            x = mod(**jo)
             l.append(x)
         return l
 
@@ -43,18 +74,25 @@ class stat:
 
     @staticmethod
     def dict2object(d, keys):
-        o = stat()
+        m = mod()
         for k in keys:
-            setattr(o, k, d[k])
-        return o
+            setattr(m, k, d[k])
+        return m
 
     @staticmethod
     def objectslice(o, keys):
-        s = stat()
+        m = mod()
         for k in keys:
             v = getattr(o, k)
-            setattr(s, k, v)
-        return s
+            setattr(m, k, v)
+        return m
+
+    @staticmethod
+    def dictslice(d, keys):
+        z = {}
+        for k in keys:
+            z[k] = d[k]
+        return z
 
 class dec:
     @staticmethod
@@ -68,5 +106,5 @@ class dec:
 
 
 if __name__ == '__main__':
-    for x in cntx('L'):
+    for x in api.cntx('L'):
         print(x.building, x.floor, x.room, x.count)
